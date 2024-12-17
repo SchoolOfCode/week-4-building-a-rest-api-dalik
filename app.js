ab4608c19e9f8f89e8de803be10d188594ee480a
Liz Wade <11866103+lizwade@users.noreply.github.com>
2024-12-17 16:40:49 +0000

    complete ticket 6

--- app.js
+++ app.js
@@ -28,13 +28,22 @@ app.get("/quotes", async function (req, res) {
 app.get("/quotes/:id", async function (req, res) {
   //use helper function to get particular ID
   res.status(200).json(await getQuoteByID(req.params.id));
-  // res.send("quotes got get got");
 });
 
 // use app.post to accept a post request
 app.post("/quotes", async function (req, res) {
   //console.log(req.body);
+  // use the body of the request to create a new quote using the addQuote helper method
+  // send a status 201
   res.status(201).send(await addQuote(req.body.quoteText, req.body.author));
 });
-// use the body of the request to create a new quote using the addQuote helper method
-// send a status 201
+
+// use app.patch to handle a patch request
+// get the quote by ID specified in the endpoint at /quotes/:id
+// set the quoteText and author to these fields in the body of the user's message
+// send a status 200
+app.patch("/quotes/:id", async function (req, res) {
+  res
+    .status(200)
+    .json(await editQuote(req.params.id, req.body.quoteText, req.body.author));
+});
