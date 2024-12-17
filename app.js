860b15269217646f06f28c5a4957467d8d1dc6f3
Kim Lida <88290393+Kinukeo@users.noreply.github.com>
2024-12-17 14:20:21 +0000

    ticket 4

--- app.js
+++ app.js
@@ -24,3 +24,9 @@ app.get("/quotes", async function (req, res) {
   //respond with all quotes from quotes.json
   res.status(200).json(await getQuotes());
 });
+
+app.get("/quotes/:id", async function (req, res) {
+  //use helper function to get particular ID
+  res.status(200).json(await getQuoteByID(req.params.id));
+  // res.send("quotes got get got");
+});
